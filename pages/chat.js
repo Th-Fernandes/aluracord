ac4059f83d4fbe64356afecdae2227c29b540c3a
unknown <Thiago Fernandes>
2022-01-28 12:01:11 -0300

    enviar  mensagem  com botão

--- pages/chat.js
+++ pages/chat.js
@@ -3,153 +3,248 @@ import React from 'react';
 import appConfig from '../config.json';
 
 export default function ChatPage() {
-    // Sua lógica vai aqui
+  // Sua lógica vai aqui
+  const [mensagem, setMensagem] = React.useState()
+  const [listaDeMensagens, setListaDeMensagens] = React.useState([])
 
-    // ./Sua lógica vai aqui
-    return (
+  function handleNovaMensagem(novaMensagem) {
+    // se a mensagem tiver ao menos um caracter, a lógica prossegue
+    if(novaMensagem.length >= 1) {
+      const mensagem = {
+        id: listaDeMensagens.length + 1,
+        de: 'th-fernandes',
+        texto: novaMensagem
+      }
+  
+      setListaDeMensagens([
+        mensagem,
+        ...listaDeMensagens,
+      ])
+      setMensagem('')
+    }
+    else {
+      console.error('não é possível enviar mensagem vazia')
+    }
+    
+  }
+  // ./Sua lógica vai aqui
+  return (
+    <Box
+      styleSheet={{
+        display: 'flex', alignItems: 'center', justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: 'url(https://wallpapercave.com/wp/wp4341592.jpg)',
+        backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        color: appConfig.theme.colors.neutrals['000']
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: '100%',
+          maxWidth: '95%',
+          maxHeight: '95vh',
+          padding: '32px',
+        }}
+      >
+        <Header />
         <Box
-            styleSheet={{
-                display: 'flex', alignItems: 'center', justifyContent: 'center',
-                backgroundColor: appConfig.theme.colors.primary[500],
-                backgroundImage: `url(https://wallpapercave.com/wp/wp4341592.jpg)`,
-                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-                color: appConfig.theme.colors.neutrals['000']
-            }}
+          styleSheet={{
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px',
+          }}
         >
+
+          <MessageList mensagens={listaDeMensagens} />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}
+          >
+            {/* 
+          listaDeMensagens.map((el) => {
+            return (
+              <li 
+              key={el.id}>
+                {el.de}:{el.texto}
+              </li>
+            )
+          })
+          */}
+            <TextField
+              value={mensagem}
+              onChange={(event) => {
+                //console.log(event)
+                return setMensagem(event.target.value)
+              }}
+              onKeyPress={(event) => {
+                if (event.key == 'Enter') {
+                  event.preventDefault()
+                  console.log('enter')
+                  //pega todos os valores anteriores ja gravadoes em listaDeMensagens, e acrescenta a nov amsg
+                  handleNovaMensagem(mensagem)
+                }
+              }
+              }
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+
             <Box
-                styleSheet={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    flex: 1,
-                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                    borderRadius: '5px',
-                    backgroundColor: appConfig.theme.colors.neutrals[700],
-                    height: '100%',
-                    maxWidth: '95%',
-                    maxHeight: '95vh',
-                    padding: '32px',
-                }}
+              styleSheet={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                width: '100%',
+              }}  
             >
-                <Header />
-                <Box
-                    styleSheet={{
-                        position: 'relative',
-                        display: 'flex',
-                        flex: 1,
-                        height: '80%',
-                        backgroundColor: appConfig.theme.colors.neutrals[600],
-                        flexDirection: 'column',
-                        borderRadius: '5px',
-                        padding: '16px',
-                    }}
-                >
 
-                    {/* <MessageList mensagens={[]} /> */}
+              <Image
+                id='enviarFigurinha'
+                src='https://cdn-icons-png.flaticon.com/512/42/42877.png'
+                styleSheet={{
+                  width: '30px',
+                  padding: '2px',
+                  backgroundColor: appConfig.theme.colors.primary[300],
+                  borderRadius: '50%',
+                  cursor: 'pointer',                 
+                }}
+              />
+              <Image
+                id='enviarMensagem'
+                onClick= {(event) => {
+                  event.preventDefault()
+                  //pega todos os valores anteriores ja gravadoes em listaDeMensagens, e acrescenta a nov amsg
+                  handleNovaMensagem(mensagem)
+                }}
+                src='https://cdn-icons-png.flaticon.com/512/81/81799.png'
+                styleSheet={{
+                  width: '30px',
+                  padding: '2px',
+                  backgroundColor: appConfig.theme.colors.primary[300],
+                  borderRadius: '50%',
+                  cursor: 'pointer',
+                  marginLeft: '12px',
+                  marginRight: '12px'
+                }}
+              />
 
-                    <Box
-                        as="form"
-                        styleSheet={{
-                            display: 'flex',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <TextField
-                            placeholder="Insira sua mensagem aqui..."
-                            type="textarea"
-                            styleSheet={{
-                                width: '100%',
-                                border: '0',
-                                resize: 'none',
-                                borderRadius: '5px',
-                                padding: '6px 8px',
-                                backgroundColor: appConfig.theme.colors.neutrals[800],
-                                marginRight: '12px',
-                                color: appConfig.theme.colors.neutrals[200],
-                            }}
-                        />
-                    </Box>
-                </Box>
+              
             </Box>
+          </Box>
         </Box>
-    )
+      </Box>
+    </Box>
+  )
 }
 
 function Header() {
-    return (
-        <>
-            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
-                <Text variant='heading5'>
-                    Chat
-                </Text>
-                <Button
-                    variant='tertiary'
-                    colorVariant='neutral'
-                    label='Logout'
-                    href="/"
-                />
-            </Box>
-        </>
-    )
+  return (
+    <>
+      <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+        <Text variant='heading5'>
+          Chat
+        </Text>
+        <Button
+          variant='tertiary'
+          colorVariant='neutral'
+          label='Logout'
+          href="/"
+        />
+      </Box>
+    </>
+  )
 }
 
 function MessageList(props) {
-    console.log('MessageList', props);
-    return (
-        <Box
-            tag="ul"
+  console.log('MessageList', props);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: '16px',
+        overflowX: 'hidden',
+      }}
+    >
+      {props.mensagens.map((mensagem) => {
+        return (
+          <Text
+            key={mensagem.id}
+            tag="li"
             styleSheet={{
-                overflow: 'scroll',
-                display: 'flex',
-                flexDirection: 'column-reverse',
-                flex: 1,
-                color: appConfig.theme.colors.neutrals["000"],
-                marginBottom: '16px',
+              borderRadius: '5px',
+              padding: '6px',
+              marginBottom: '12px',
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              }
             }}
-        >
-
-            <Text
-                key={mensagem.id}
-                tag="li"
-                styleSheet={{
-                    borderRadius: '5px',
-                    padding: '6px',
-                    marginBottom: '12px',
-                    hover: {
-                        backgroundColor: appConfig.theme.colors.neutrals[700],
-                    }
-                }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: '8px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
             >
-                <Box
-                    styleSheet={{
-                        marginBottom: '8px',
-                    }}
-                >
-                    <Image
-                        styleSheet={{
-                            width: '20px',
-                            height: '20px',
-                            borderRadius: '50%',
-                            display: 'inline-block',
-                            marginRight: '8px',
-                        }}
-                        src={`https://github.com/vanessametonini.png`}
-                    />
-                    <Text tag="strong">
-                        {mensagem.de}
-                    </Text>
-                    <Text
-                        styleSheet={{
-                            fontSize: '10px',
-                            marginLeft: '8px',
-                            color: appConfig.theme.colors.neutrals[300],
-                        }}
-                        tag="span"
-                    >
-                        {(new Date().toLocaleDateString())}
-                    </Text>
-                </Box>
-                {mensagem.texto}
-            </Text>
-        </Box>
-    )
+              <Image
+                styleSheet={{
+                  width: '20px',
+                  height: '20px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  marginRight: '8px',
+                }}
+                src={`https://github.com/vanessametonini.png`}
+              />
+              <Text tag="strong">
+                {mensagem.de}
+              </Text>
+              <Text
+                styleSheet={{
+                  fontSize: '10px',
+                  marginLeft: '8px',
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+               {(new Date().toLocaleTimeString())} - {(new Date().toLocaleDateString())} 
+              </Text>
+            </Box>
+            {mensagem.texto}
+          </Text>
+        )
+      })}
+
+    </Box>
+  )
 }
